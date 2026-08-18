b2bcff3f4e77e3a5d552e77211736dfd98c3af48
Javier Murillo <jajomuri@espol.edu.ec>
2026-08-18 04:24:05 +0000

    cambio en la ruta de assets

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
 ]
 
-STATIC_ROOT = "assets/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Coloque la ruta relativa al archivo con la clave privada
 FIREBASE_CREDENTIALS_PATH = credentials.Certificate("secrets/landing-key.json")
